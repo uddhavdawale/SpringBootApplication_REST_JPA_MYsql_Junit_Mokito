074a8150b78e1d4c93e8550d54e04c07663324e6
Abc <uddhavdawale2@gmail.com>
2023-09-10 19:50:42 +0530

    first commit after

---
+++ src/main/java/com/ud/demo/model/Details.java
@@ -0,0 +1,25 @@
+package com.ud.demo.model;
+
+/**
+ * @author Raje
+ * just for demo purpose adding 
+ */
+public class Details 
+{
+	private String detailsmsg;
+
+	
+	public Details(String detailsmsg) {
+		this.detailsmsg = detailsmsg;
+	}
+
+	public String getDetailsmsg() {
+		return detailsmsg;
+	}
+
+	public void setDetailsmsg(String detailsmsg) {
+		this.detailsmsg = detailsmsg;
+	}
+	
+
+}
